69ec8fdb382f21a43af1acba29cd2b013465cad3
klauslube <klauslube93@gmail.com>
2023-05-03 12:45:59 -0300

    feat: criacao de join table customerorder

---
+++ db/migrate/20230503152214_create_join_table_customers_orders.rb
@@ -0,0 +1,11 @@
+class CreateJoinTableCustomersOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :customers, :orders do |t|
+      t.references :coupon, null: false, foreign_key: true
+      t.references :payment, null: false, foreign_key: true
+
+      t.index [:customer_id, :order_id], unique: true
+
+    end
+  end
+end
