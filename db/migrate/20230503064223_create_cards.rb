6a22221686d03d9c091589525eec583c31b2e5a8
klauslube <klauslube93@gmail.com>
2023-05-03 12:58:18 -0300

    feat: adiçao de modificador unique para atributos

--- db/migrate/20230503064223_create_cards.rb
+++ db/migrate/20230503064223_create_cards.rb
@@ -3,7 +3,7 @@ class CreateCards < ActiveRecord::Migration[7.0]
     create_table :cards do |t|
       t.references :customer, null: false, foreign_key: true
       t.string :name, null: false, limit: 255
-      t.string :card_number, null: false, limit: 20
+      t.string :card_number, null: false, limit: 20, unique: true
       t.string :CVV_code, null: false, limit: 4
       t.integer :card_type, null: false
       t.string :expiration_date, null: false
--- db/migrate/20230503064506_create_coupons.rb
+++ db/migrate/20230503064506_create_coupons.rb
@@ -2,7 +2,7 @@ class CreateCoupons < ActiveRecord::Migration[7.0]
   def change
     create_table :coupons do |t|
       t.references :administrator, null: false, foreign_key: true
-      t.string :code, null: false, limit: 255
+      t.string :code, null: false, limit: 255, unique: true
       t.string :name, null: false, limit: 255
       t.text :description, null: false
       t.date :start_date, null: false
